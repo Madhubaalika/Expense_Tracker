9c698807e647e4bd80f7c5fa2e5fd5de8c8d9b05
Madhubaalika <madhubaalika@gmail.com>
2024-07-02 13:51:15 +0530

    changes to DB.js...

--- config/DB.js
+++ config/DB.js
@@ -1,16 +1,12 @@
-require('dotenv').config();
 const { Sequelize } = require('sequelize');
+const pass=require('../config/pass');
+const sequelize = new Sequelize('expensetracker', 'root',pass, {
+  host:'localhost',
+  dialect:'mysql'
+});
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
-  }
-);
 sequelize.authenticate()
   .then(() => console.log('Database connected successfully'))
   .catch(err => console.log('Error:' + err));
+
 module.exports = sequelize;
